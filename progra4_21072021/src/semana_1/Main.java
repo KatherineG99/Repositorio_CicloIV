acf89238feec76668292ef0813491ea1f765e708
KatherineG99 <73448453+KatherineG99@users.noreply.github.com>
2021-12-14 17:58:47 -0600

    Add files via upload

---
+++ progra4_21072021/src/semana_1/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana_1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Puerta puertita = new Puerta();
+        
+        puertita.abrir();
+    }
+}
---
+++ progra4_21072021/src/semana_1/Puerta.java
@@ -0,0 +1,20 @@
+
+package semana_1;
+
+
+public class Puerta {
+    String color;
+    int ancho;
+    int altura;
+    
+    
+    public void abrir(){
+        System.out.println("estoy abriendo");
+    }
+    
+    public void cerrar(){
+        System.out.println("estoy cerrando");
+    }
+}
+
+
